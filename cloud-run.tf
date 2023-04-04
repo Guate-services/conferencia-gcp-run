658b44950a468d47fbd8b3f5465c151895be989b
fran2344 <ardonfrancisco456@gmail.com>
2023-04-03 22:27:16 -0600

    Adding cloud run validation

--- cloud-run.tf
+++ cloud-run.tf
@@ -1,10 +1,14 @@
 resource "google_cloud_run_service" "default" {
   name     = "pubsub-tutorial"
-  location = "us-central1"
+  location = local.region
   template {
     spec {
       containers {
         image = "gcr.io/rosy-timer-381919/pubsub"
+        env {
+          name = "BLURRED_BUCKET_NAME"
+          value = google_storage_bucket.imageproc_output.name
+        }
       }
     }
   }
